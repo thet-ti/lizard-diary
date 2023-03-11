cf8a03dc8076899279d1ecf9b858b3489ace23a5
Mikha Thet <social.amais@gmail.com>
2023-03-11 20:17:20 -0300

    chore: navigation

---
+++ src/router/Router.tsx
@@ -0,0 +1,16 @@
+import * as React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {HomeScreen} from '../screens/HomeScreen';
+
+export default function Router() {
+  const Stack = createNativeStackNavigator();
+
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
