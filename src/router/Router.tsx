16662a4ef8b44db77039c746d313a6ea49467295
Mikha Thet <social.amais@gmail.com>
2023-03-11 23:12:11 -0300

    fix: icon size

--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -1,15 +1,20 @@
 import * as React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import {HomeScreen} from '../screens/HomeScreen';
+import {HomeScreen} from '../screens/home/HomeScreen';
+import {ROUTES} from './routes';
 
 export default function Router() {
   const Stack = createNativeStackNavigator();
 
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
+      <Stack.Navigator
+        initialRouteName={ROUTES.HOME_SCREEN}
+        screenOptions={{
+          headerShown: false,
+        }}>
+        <Stack.Screen name={ROUTES.HOME_SCREEN} component={HomeScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
